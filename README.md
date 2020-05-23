52d99d86bd88320d801220e8c9291ff433eab85d
Victor Espaillat <vicander_99@hotmail.com>
2020-05-23 16:06:03 -0400

    Events & event handling (rolling dice)

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
 -   Manipulating the DOM.
 -   Reading from the DOM.
 -   Changing CSS styles.
+
+### Events and Event Handling (rolling the dice)
+
+-   Setting up an event handler.
+-   Callback functions.
+-   Anoynymous functions.
+-   Selecting elements by id.
+-   Changing the image in an `<img>` element.
--- app.js
+++ app.js
@@ -21,4 +21,15 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 1;
 
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
 document.querySelector(".dice").style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", () => {
+    let dice = Math.floor(Math.random() * 6) + 1;
+    let diceDOM = document.querySelector(".dice");
+    diceDOM.style.display = "block";
+    diceDOM.src = "./images/dice-" + dice + ".png";
+});
