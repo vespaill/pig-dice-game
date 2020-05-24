fa69f4b2de7a94f0b150d04f008c1d312da4555a
Victor Espaillat <vicander_99@hotmail.com>
2020-05-24 08:25:41 -0400

    Game initialization function

--- app.js
+++ app.js
@@ -15,12 +15,10 @@
  * The first player to reach 100 points on GLOBAL score wins the game.
  */
 
-let scores, roundScore, activePlayer;
 const WIN_SCORE = 20;
+let scores, roundScore, activePlayer;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
 document.querySelector(".dice").style.display = "none";
 document.querySelector(".btn-roll").addEventListener("click", () => {
@@ -73,3 +71,29 @@ function nextPlayer() {
 
     document.querySelector(".dice").style.display = "none";
 }
+
+function init() {
+    scores = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+
+    document.querySelector(".dice").style.display = "none";
+
+    document.getElementById("score-0").textContent = '0';
+    document.getElementById("score-1").textContent = '0';
+    document.getElementById("current-0").textContent = '0';
+    document.getElementById("current-1").textContent = '0';
+
+    document.getElementById("name-0").textContent = "Player 1";
+    document.getElementById("name-1").textContent = "Player 2";
+
+    document.querySelector(".player-0-panel").classList.remove("winner");
+    document.querySelector(".player-1-panel").classList.remove("winner");
+    document.querySelector(".player-0-panel").classList.remove("active");
+    document.querySelector(".player-1-panel").classList.remove("active");
+
+    document.querySelector(".player-0-panel").classList.add("active");
+
+}
+
+document.querySelector(".btn-new").addEventListener("click", init);
